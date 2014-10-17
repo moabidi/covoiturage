c1e603f7e628e2c8261412b822222738ec8aa088
mabidi <m.abidi@novactive.com>
2014-10-17 18:03:14 +0100

    block recherche

--- Symfony/src/Covoiturage/FrontendBundle/Controller/DefaultController.php
+++ Symfony/src/Covoiturage/FrontendBundle/Controller/DefaultController.php
@@ -9,6 +9,8 @@ use Covoiturage\FrontendBundle\Entity\Pays;
 use Covoiturage\FrontendBundle\Entity\Gouvernorat;
 use Covoiturage\FrontendBundle\Entity\Delegation;
 use Covoiturage\FrontendBundle\Entity\voyage;
+use Doctrine\Common\Collections\Expr;
+use Doctrine\Common\Collections\Criteria;
 
 
 class DefaultController extends Controller
@@ -43,5 +45,26 @@ class DefaultController extends Controller
     public function getConnectionAction()
     {
 
+    }
+
+    /**
+     * @return Response
+     */
+    public function getAllGouvernoratsAction()
+    {
+        $listGouvernorats = $this->getDoctrine()->getRepository('CovoiturageFrontendBundle:gouvernorat')->findAll();
+        //var_dump($listOffres);
+        return $this->render('CovoiturageFrontendBundle:_common:selection.html.twig', array('list_items' => $listGouvernorats));
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function getAllDelegationsAction($id = null)
+    {
+        $listDelegations = $this->getDoctrine()->getRepository('CovoiturageFrontendBundle:delegation')->matching( new Criteria( new Expr\Comparison( "idGouvernorat",Expr\Comparison::EQ,"2")));
+        //var_dump($listOffres);
+        return $this->render('CovoiturageFrontendBundle:_common:selection.html.twig', array('list_items' => $listDelegations));
     }
 }
