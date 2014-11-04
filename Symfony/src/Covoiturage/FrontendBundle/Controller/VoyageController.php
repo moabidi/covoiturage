de8db41446b3065f685bc3b0e46855ed0c5ed194
Wissem Riahi <wissemr@gmail.com>
2014-11-05 00:19:37 +0100

    Add Form type for voyage - wip

--- Symfony/src/Covoiturage/FrontendBundle/Controller/VoyageController.php
+++ Symfony/src/Covoiturage/FrontendBundle/Controller/VoyageController.php
@@ -9,6 +9,8 @@
 
 namespace Covoiturage\FrontendBundle\Controller;
 
+use Covoiturage\FrontendBundle\Entity\Voyage;
+use Covoiturage\FrontendBundle\Form\Type\VoyageType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,10 +19,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class VoyageController extends Controller
 {
-    public function publishAction()
+    public function publishAction(Request $request)
     {
+        $voyage = new Voyage();
 
-        return $this->render('CovoiturageFrontendBundle:Voyage:publish.html.twig');
+        $form = $this->get('form.factory')->create(new VoyageType(), $voyage);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($voyage);
+            $em->flush();
+
+            //TODO:
+            // add session message
+            // redirect to Voyage view
+        }
+
+        return $this->render('CovoiturageFrontendBundle:Voyage:publish.html.twig',
+                    array(
+                        'form' => $form->createView()
+                    )
+        );
     }
 
 
