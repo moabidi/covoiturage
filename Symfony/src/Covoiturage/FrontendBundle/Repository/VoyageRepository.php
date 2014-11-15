9629a0a129f6072ba0f68439c821dcdd6e3ea525
Wissem Riahi <wissemr@gmail.com>
2014-11-15 21:21:19 +0100

    Add user navette listing, close #6

--- Symfony/src/Covoiturage/FrontendBundle/Repository/VoyageRepository.php
+++ Symfony/src/Covoiturage/FrontendBundle/Repository/VoyageRepository.php
@@ -20,7 +20,6 @@ class VoyageRepository extends EntityRepository
      *
      * @param int $page
      * @param int $maxperpage
-     * @param string $sortby
      * @return Paginator
      */
     public function getList($page=1, $maxperpage=10)
@@ -47,6 +46,50 @@ class VoyageRepository extends EntityRepository
         $qb = $this->_em->createQueryBuilder()
             ->select('count(voyage.id)')
             ->from('CovoiturageFrontendBundle:Voyage','voyage')
+        ;
+
+        $count = $qb->getQuery()->getSingleScalarResult();
+
+        return $count;
+    }
+
+    /**
+     * Get the list of current user voyages
+     *
+     * @param int $user
+     * @param int $page
+     * @param int $maxperpage
+     * @return Paginator
+     */
+    public function getUserList($user,$page=1, $maxperpage=10)
+    {
+        $qb = $this->_em->createQueryBuilder()
+            ->select('voyage')
+            ->from('CovoiturageFrontendBundle:Voyage','voyage')
+            ->where('voyage.utilisateur = :user')
+            ->orderBy('voyage.id','DESC')
+            ->setParameter('user',$user)
+        ;
+
+        $qb->setFirstResult(($page-1) * $maxperpage)
+            ->setMaxResults($maxperpage);
+
+        return new Paginator($qb);
+    }
+
+    /**
+     * Get the number of Voyages
+     *
+     * @param int $user
+     * @return int
+     */
+    public function countUserVoyages($user)
+    {
+        $qb = $this->_em->createQueryBuilder()
+            ->select('count(voyage.id)')
+            ->from('CovoiturageFrontendBundle:Voyage','voyage')
+            ->where('voyage.utilisateur=:user')
+            ->setParameter('user',$user)
         ;
 
         $count = $qb->getQuery()->getSingleScalarResult();
